b0db8d097245c31ab37e24acd46f2b8c0e502118
r4gamia <17311182+rg4amia@users.noreply.github.com>
2022-05-31 16:16:37 +0000

    manage project

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-            <a class="navbar-brand" href="/livre"><img src="https://fisatevent.com/wp-content/uploads/2022/04/Logo_FISAT.png" data-at2x="https://fisatevent.com/wp-content/uploads/2022/04/Logo_FISAT.png" style="width: 70px !important; height: 70px" alt="Logo Weblogy"></a>
+            <a class="navbar-brand" href=""><img src="https://fisatevent.com/wp-content/uploads/2022/04/Logo_FISAT.png" data-at2x="https://fisatevent.com/wp-content/uploads/2022/04/Logo_FISAT.png" style="width: 70px !important; height: 70px" alt="Logo Weblogy"></a>
         </div>
         <div class="collapse navbar-collapse" id="bs-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
@@ -27,7 +27,7 @@
                 </li>
                 <li><a href="/livre/points-vente">Points de vente</a></li>
                 <li><a href="/livre#contact">Contacts</a></li>--}}
-                <li><a href="" class="btn-success btn btn-rounded text-info">Retour au Site</a></li>
+                <li><a href="https://fisatevent.com/" class="btn-success btn btn-rounded text-info">Retour au Site</a></li>
             </ul>
         </div>
     </div>
